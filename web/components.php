1b8b1128390d80f83e9d0e3ebaf872586f995c76
jhm69 <jahangirhmarup@gmail.com>
2023-05-18 01:00:16 +0600

    added admin feature

--- web/components.php
+++ web/components.php
@@ -1,8 +1,9 @@
 <?php
 include_once 'functions.php';
-function loginbox() {
+function loginbox()
+{
     if (!isset($_SESSION['loggedin'])) {
-        ?>
+?>
         <div class="panel-heading text-center">
             <h3 class="panel-title">Login</h3>
         </div>
@@ -11,19 +12,19 @@ function loginbox() {
             <form action="<?php echo SITE_URL; ?>/process.php" method="post" role="form">
                 <div class="input-group" style="margin-bottom: -1px;">
                     <span class="input-group-addon" style="border-bottom-left-radius: 0;"><i class="glyphicon glyphicon-user"></i></span>
-                    <input class="form-control" style="border-bottom-right-radius: 0;" type="text" name="teamname" placeholder="Teamname" required/>
+                    <input class="form-control" style="border-bottom-right-radius: 0;" type="text" name="teamname" placeholder="Teamname" required />
                 </div>
                 <div class="input-group">
                     <span style="border-top-left-radius: 0;" class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                    <input style="border-top-right-radius: 0;" class="form-control" type="password" name="password" placeholder="Password" required/>
-                </div><br/>
-                <input type="submit" name="login" value="Log In" class="btn btn-primary btn-block"/>
+                    <input style="border-top-right-radius: 0;" class="form-control" type="password" name="password" placeholder="Password" required />
+                </div><br />
+                <input type="submit" name="login" value="Log In" class="btn btn-primary btn-block" />
             </form>
             <a href='<?php echo SITE_URL; ?>/register'>New Team? Register Here.</a>
         </div>
-        <?php
+    <?php
     } else {
-        ?>
+    ?>
         <div class="panel-heading text-center">
             <h3 class="panel-title">Team</h3>
         </div>
@@ -44,25 +45,63 @@ function loginbox() {
                 ?>
             </table>
         </div>
-        <?php
+    <?php
     }
 }
 
-function errorMessageHTML($msg){
-    return '<br /><div class="alert alert-danger" role="alert">'.$msg.'</div>';
+function pagination($noofpages, $url, $page, $maxcontent)
+{
+    if ($noofpages > 1) {
+        if ($page - ($maxcontent / 2) > 0)
+            $start = $page - 5;
+        else
+            $start = 1;
+        if ($noofpages >= $start + $maxcontent)
+            $end = $start + $maxcontent;
+        else
+            $end = $noofpages;
+    ?>
+        <div align='center'>
+            <ul class="pagination">
+                <?php if ($page > 1) { ?>
+                    <li><a href="<?php echo $url . "&page=1"; ?>">First</a></li>
+                    <li><a href="<?php echo $url . "&page=" . ($page - 1); ?>">Prev</a></li>
+                <?php
+                }
+                for ($i = $start; $i <= $end; $i++) {
+                ?>
+                    <li <?php echo ($i == $page) ? ("class='disabled'") : (''); ?>><a href="<?php echo ($i != $page) ? ($url . "&page=" . $i) : ("#"); ?>"><?php echo $i; ?></a></li>
+                <?php
+                }
+                if ($page < $noofpages) {
+                ?>
+                    <li><a href="<?php echo $url . "&page=" . ($page + 1); ?>">Next</a></li>
+                    <li><a href="<?php echo $url . "&page=" . $noofpages; ?>">Last</a></li>
+                <?php } ?>
+            </ul>
+        </div>
+<?php
+    }
 }
 
-function doCompetitionCheck(){
+
+function errorMessageHTML($msg)
+{
+    return '<br /><div class="alert alert-danger" role="alert">' . $msg . '</div>';
+}
+
+function doCompetitionCheck()
+{
     //Automates starting of planned contests
     //No need for a cron job since nobody's gunna push code if they're not on the site anyways
     $query = "select value from admin where variable = 'mode'";
     $result = DB::findOneFromQuery($query);
-    if($result['value'] == 'Passive') {
+    if ($result['value'] == 'Passive') {
         $curTime = time();
         $query = "select endtime from contest where endtime >= $curTime and starttime<=$curTime";
         $result = DB::findOneFromQuery($query);
-        if(isset($result['endtime'])) {
-            $admin = Array();
+        if (isset($result['endtime'])) {
+            $admin = array();
             $admin['mode'] = 'Active';
             $admin['endtime'] = $result['endtime'];
             foreach ($admin as $key => $val) {
--- web/navigation.php
+++ web/navigation.php
@@ -60,7 +60,7 @@
                     ) { ?>
 
                       <li>
-                        <a href='/adminjudge' class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                        <a href='<?php echo SITE_URL; ?>/adminjudge.php' class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                           <span class="flex-1 ml-3 whitespace-nowrap">Judge Settings</span>
                         </a>
                       </li>
@@ -71,7 +71,7 @@
                       </li>
 
                       <li>
-                        <a href='/admincontest' class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                        <a href='<?php echo SITE_URL; ?>/admin_contests.php' class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                           <span class="flex-1 ml-3 whitespace-nowrap">Contest Settings</span>
                         </a>
                       </li>
@@ -81,11 +81,6 @@
                         </a>
                       </li>
 
-                      <li>
-                        <a href='/admingroup' class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                          <span class="flex-1 ml-3 whitespace-nowrap">Group Settings</span>
-                        </a>
-                      </li>
                       <li>
                         <a href='/adminclar' class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                           <span class="flex-1 ml-3 whitespace-nowrap">Clarifications</span>
