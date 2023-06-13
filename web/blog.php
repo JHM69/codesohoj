2f9847500ea2b26dab3bec44bf3db7b7bd5c6c66
jhm69 <jahangirhmarup@gmail.com>
2023-06-14 04:05:36 +0600

    fix

--- web/blog.php
+++ web/blog.php
@@ -5,71 +5,70 @@ require_once "config.php";
 require_once "navigation.php";
 include_once "functions.php";
 
-include_once "functions.php";
 if (
     isset($_SESSION["loggedin"]) &&
     $_SESSION["Users"]["status"] == "Normal" || $_SESSION["Users"]["status"] == "Admin"
 ) { ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog | CodeSohoj</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.17/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Blog | CodeSohoj</title>
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.17/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="styles.css">
+    </head>
 
-<body class="bg-gray-100">
-    <div class="container mx-auto py-8">
-        <h1 class="text-3xl font-bold mb-4">Blog</h1>
+    <body class="bg-gray-100">
+        <div class="container mx-auto py-8">
+            <h1 class="text-3xl font-bold mb-4">Blog</h1>
 
-        <!-- Blog posts -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
-                <h2 class="text-xl font-bold mb-2">Getting Started with Competitive Programming</h2>
-                <p>Learn how to get started with competitive programming and improve your problem-solving skills.</p>
-                <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+            <!-- Blog posts -->
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
+                    <h2 class="text-xl font-bold mb-2">Getting Started with Competitive Programming</h2>
+                    <p>Learn how to get started with competitive programming and improve your problem-solving skills.</p>
+                    <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+                </div>
+
+                <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
+                    <h2 class="text-xl font-bold mb-2">Mastering Dynamic Programming</h2>
+                    <p>Explore the concept of dynamic programming and how it can be used to solve complex problems efficiently.</p>
+                    <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+                </div>
+
+                <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
+                    <h2 class="text-xl font-bold mb-2">The Art of Problem Solving</h2>
+                    <p>Discover the art of problem solving and learn strategies to tackle challenging programming problems.</p>
+                    <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+                </div>
+
+                <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
+                    <h2 class="text-xl font-bold mb-2">Introduction to Web Development</h2>
+                    <p>Get started with web development and learn the basics of HTML, CSS, and JavaScript.</p>
+                    <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+                </div>
+
+                <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
+                    <h2 class="text-xl font-bold mb-2">Understanding Big O Notation</h2>
+                    <p>Learn about Big O notation and how it measures the efficiency of algorithms.</p>
+                    <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+                </div>
+
+                <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
+                    <h2 class="text-xl font-bold mb-2">Database Management with SQL</h2>
+                    <p>Discover the world of databases and learn how to manage data using SQL.</p>
+                    <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+                </div>
             </div>
-
-            <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
-                <h2 class="text-xl font-bold mb-2">Mastering Dynamic Programming</h2>
-                <p>Explore the concept of dynamic programming and how it can be used to solve complex problems efficiently.</p>
-                <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
-            </div>
-
-            <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
-                <h2 class="text-xl font-bold mb-2">The Art of Problem Solving</h2>
-                <p>Discover the art of problem solving and learn strategies to tackle challenging programming problems.</p>
-                <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
-            </div>
-
-            <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
-                <h2 class="text-xl font-bold mb-2">Introduction to Web Development</h2>
-                <p>Get started with web development and learn the basics of HTML, CSS, and JavaScript.</p>
-                <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
-            </div>
-
-            <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
-                <h2 class="text-xl font-bold mb-2">Understanding Big O Notation</h2>
-                <p>Learn about Big O notation and how it measures the efficiency of algorithms.</p>
-                <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
-            </div>
-
-            <div class="bg-white rounded-lg shadow-lg p-6 hover:bg-blue-100 transition-colors duration-300">
-                <h2 class="text-xl font-bold mb-2">Database Management with SQL</h2>
-                <p>Discover the world of databases and learn how to manage data using SQL.</p>
-                <a href="view_blog.php?blog=getting_started_cp" target="_blank" class="text-blue-500">Read More</a>
+            <div class="bg-gray-100 py-4 mt-4">
+                <div class="container mx-auto flex justify-center">
+                    <a href="add_blog.php" target="_blank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Add Blog
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="bg-gray-100 py-4 mt-4">
-            <div class="container mx-auto flex justify-center">
-                <a href="add_blog.php" target="_blank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Add Blog
-                </a>
-            </div>
-        </div>
-    </div>
-</body>
+    </body>
 
 </html>
 <?php } ?>
--- web/process.php
+++ web/process.php
@@ -342,7 +342,7 @@ if (isset($_POST["login"])) {
           }
           fwrite($client, $rid);
           fclose($client);
-          redirectTo(SITE_URL . "/viewsolution/" . $rid);
+          redirectTo(SITE_URL . "/viewsolution.php?code=" . $rid);
         } else {
           DB::query("Delete from runs where rid = $rid");
           $_SESSION['msg'] = "Some error occured during submission. If the problem continues contact Admin";
