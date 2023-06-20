42b149ad4bcbeee0e9c8c7381fd3a577e36f3090
jhm69 <jahangirhmarup@gmail.com>
2023-06-21 03:49:54 +0600

    editor bug fixed

--- judge/env/code.cpp
+++ judge/env/code.cpp
@@ -1,14 +1,18 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int x;
-        cin >> x;
-        int result = 2 * x;
-        cout << result << endl;
+int main()
+{
+    int n, k, i, sum;
+    while(~scanf("%d%d",&n, &k))
+    {
+        sum = k;
+        for(i = 0; i <= n; i ++)
+        {
+            sum += i * 5;
+            if(sum > 240)
+                break;
+        }
+        printf("%d\n", i - 1);
     }
     return 0;
 }
-
